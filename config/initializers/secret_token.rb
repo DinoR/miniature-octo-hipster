54e47130822a1fed5e1e7ac3cece711cd3bf924b
Dino R <dino.rosic@me.com>
2013-11-24 17:55:41 +0100

    Rails p.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MiniatureOctoHipster::Application.config.secret_key_base = 'd87191de394461deafdcd723e96278c618472b7442c519020edf1793d596a38730b5200a2094d3b5cf69b9ee5685712f2689abc6eed410ffbd3665d092a410a0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MiniatureOctoHipster::Application.config.session_store :cookie_store, key: '_miniature-octo-hipster_session'
